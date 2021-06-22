5f31b64f77deb0eaf90fdce57c52dfcbda1ef6b0
leftbezero <leftbezero@gmail.com>
2021-06-22 00:47:14 -0400

    Enjoy chatting with strangers

--- pyomegle2021/user.py
+++ pyomegle2021/user.py
@@ -12,3 +12,14 @@ import time
 h = OmegleHandler(loop=True)            # session loop
 c = OmegleClient(h, wpm=47, lang='en')  # 47 words per minute
 c.start()
+
+while 1:
+    input_str = input('')           # string input
+
+    if input_str.strip() == '/next':
+        c.next()                        # new conversation
+    elif input_str.strip() == '/exit':
+        c.disconnect()                  # disconnect chat session
+        break
+    else:
+        c.send(input_str)               # send string
